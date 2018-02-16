a1389243447e7a12b673d5597a6e8d10a9819383
William Gomez <william.gomex@gmail.com>
2018-02-16 12:51:45 -0500

    Programa general de la piramide

--- EjemploClase/main.cpp
+++ EjemploClase/main.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 int main()
 {
-    cout << "H  ello World! " << endl;
-    return 0;
+    int i;
+
+    char letraultim='Z';
+    int renglones=letraultim-'A'+1;
+    for(i=0;i<renglones;i++){
+        int j;
+        char letra='A';
+        for(j=0;j<=i;j++){
+            cout << letra;
+            letra++;
+        }
+
+        for(j=(2*renglones-3)-2*i;j>=1;j--){
+            cout << " ";
+        }
+
+        letra--;
+        for(j=0;j<=i;j++){
+            if(letra != letraultim){
+                cout << letra;
+            }
+            letra--;
+        }
+        cout << endl;
+    }
 }
